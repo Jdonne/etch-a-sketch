15fe729d84b645e86ecc8ec1c0d1a64199c3a584
Eric Tien <eric.tien.13@gmail.com>
2019-11-14 13:29:54 -0500

    add hover coloring, add reset and grid dimension prompt

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
+let n = 32
 function gridmaker() {
-for (let r = 0; r<256; r++) {
-    const div = document.createElement('div');
-    div.style.width = `${50/16}em`;
-    div.style.height = `${50/16}em`;
-    div.className = 'grid-block';
-    container.appendChild(div);
-}
+    for (let r = 0; r < (Math.pow(n, 2)); r++) {
+        const div = document.createElement('div');
+        div.style.width = `${50 / n}em`;
+        div.style.height = `${50 / n}em`;
+        div.className = 'grid-block';
+        container.appendChild(div);
+    }
 }
 gridmaker();
+
+function fillIn(e) {
+    let griddiv = e.target
+    griddiv.classList.add('filled');
+}
+function reset() {
+    const filledBlock = document.querySelectorAll('.filled');
+    //removes all blocks
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    //removes colored blocks
+    filledBlock.forEach(b => b.classList.remove('filled'));
+    //
+    n = window.prompt("width and height of grid?");
+    gridmaker();
+}
+container.addEventListener('mouseover', fillIn)
+button.addEventListener('click', reset)
